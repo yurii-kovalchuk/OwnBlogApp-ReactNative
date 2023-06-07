759adb996a89d74419f99ac16d230f30f8c6673f
yurii-kovalchuk <sogdrums@gmail.com>
2023-06-07 16:09:59 +0300

    add markup register and login forms

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,7 +1,17 @@
-import { View } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
 const LoginScreen = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>Увійти</Text>
+      <View>
+        <TextInput placeholder="Адреса електронної пошти" />
+        <TextInput placeholder="Пароль" />
+      </View>
+      <Button title="Увійти" />
+      <Text>Немає акаунту? Зареєструватися</Text>
+    </View>
+  );
 };
 
 export default LoginScreen;
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,7 +1,19 @@
-import { View } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
 const RegistrationScreen = () => {
-  return <View></View>;
+  return (
+    <View>
+      <View></View>
+      <Text>Реєстрація</Text>
+      <View>
+        <TextInput placeholder="Логін" />
+        <TextInput placeholder="Адреса електронної пошти" />
+        <TextInput placeholder="Пароль" />
+      </View>
+      <Button title="Зареєстуватися" />
+      <Text>Вже є акаунт? Увійти</Text>
+    </View>
+  );
 };
 
 export default RegistrationScreen;
